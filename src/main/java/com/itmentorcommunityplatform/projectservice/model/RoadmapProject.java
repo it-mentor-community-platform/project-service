788c5f878132ebf0aeccece0dbd98203db244c81
artemka2003 <86artem2003@mail.ru>
2026-09-16 20:51:16 +0300

    fix: добавлен распарс входного типа проекта в enum
    в json приходит CURRENCY-EXCHANGE и записывается в enum

--- src/main/java/com/itmentorcommunityplatform/projectservice/model/RoadmapProject.java
+++ src/main/java/com/itmentorcommunityplatform/projectservice/model/RoadmapProject.java
@@ -1,5 +1,7 @@
 package com.itmentorcommunityplatform.projectservice.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum RoadmapProject {
     HANGMAN,
     SIMULATION,
@@ -8,5 +10,24 @@ public enum RoadmapProject {
     WEATHER_VIEWER,
     CLOUD_FILE_STORAGE,
     TASK_TRACKER,
-    OTHER
+    OTHER;
+
+
+    @JsonCreator
+    public static RoadmapProject from(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        String normalized = value.replace("-", "_");
+
+        for (RoadmapProject type : values()) {
+            if (type.name().equalsIgnoreCase(normalized)) {
+                return type;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown RoadmapProjectType: " + value);
+    }
+
 }
